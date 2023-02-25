7f2d48def783cc4a851fb74aab916632027b4753
Pete <pisces72297@gmail.com>
2023-02-25 22:25:12 +0800

    no message

--- app/http.go
+++ app/http.go
@@ -1,20 +1,33 @@
 package app
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func demoHttp() {
-	resp, err := http.Get("https://www.google.com/")
-	//client := &http.Client{
-	//	Timeout: 1 * time.Second,
-	//}
-	//req, err := http.NewRequest("GET", "https://www.google.com/", nil)
-	//ctx, cancel := context.WithTimeout(context.Background(), 1*time.Microsecond)
-	//req = req.WithContext(ctx)
-	//resp, err := client.Do(req)
-	fmt.Println(err)
-	fmt.Println(resp)
-	//cancel()
+	root, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	run := func(ctx context.Context) {
+		//resource := "https://www.google.com/"
+		resource := "https://localhost:9110/api/default/run"
+		//resp, err := http.Get(resource)
+		client := &http.Client{
+			//Timeout: 1 * time.Second,
+		}
+		req, err := http.NewRequest("GET", resource, nil)
+		req = req.WithContext(ctx)
+		resp, err := client.Do(req)
+		fmt.Println("done")
+		fmt.Println(err)
+		body, err := ioutil.ReadAll(resp.Body)
+		fmt.Println(err)
+		fmt.Println(string(body))
+	}
+	go run(root)
+	time.Sleep(1 * time.Second)
+	cancel()
+	fmt.Println("cancel")
 }
